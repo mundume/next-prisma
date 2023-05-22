89c3a2a437bda50f40b2b773ef16eb69e49b58ac
nzai <nzaih18@gmail.com>
2023-05-22 14:03:58 +0300

    profile page dynamic routing

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,3 +1,23 @@
-export default function page() {
-  return <div>page</div>;
+import { prisma } from "@/lib/prisma";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function page({ params }: Props) {
+  const userData = await prisma.user.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+
+  return (
+    <div className="grid gap-1 p-2">
+      <img src={userData?.image!} alt={userData?.name!} />
+      <small className="font-bold">{userData?.email!}</small>
+      <p className="font-semibold text-purple-600">{userData?.name}</p>
+    </div>
+  );
 }
