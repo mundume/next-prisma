d9fa7440ea5119e9319240bcb2b41476ff0853c1
nzai <nzaih18@gmail.com>
2023-06-07 17:45:59 +0300

    small tings

--- app/post/addpost.tsx
+++ app/post/addpost.tsx
@@ -25,22 +25,24 @@ export default function AddPosts() {
     console.log(response);
   };
   return (
-    <div>
-      <form action="" onSubmit={addPost} className="flex items-center gap-1">
-        <input
-          ref={ref}
-          placeholder="What's Happening?"
-          name="title"
-          className="w-full border border-yellow-500 textarea textarea-lg"
-        />
-        <button
-          type="submit"
-          className="px-4 py-3 font-semibold text-white bg-purple-600 border-none rounded-md outline-none hover:bg-purple-400 active:bg-purple-500"
-        >
-          {" "}
-          Post
-        </button>
-      </form>
+    <div className="flex items-center justify-center">
+      <div>
+        <form action="" onSubmit={addPost} className="flex items-center gap-1">
+          <input
+            ref={ref}
+            placeholder="What's Happening?"
+            name="title"
+            className="w-full border border-yellow-500 textarea textarea-lg"
+          />
+          <button
+            type="submit"
+            className="px-4 py-3 font-semibold text-white bg-purple-600 border-none rounded-md outline-none hover:bg-purple-400 active:bg-purple-500"
+          >
+            {" "}
+            Post
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
