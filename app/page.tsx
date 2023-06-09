01dfdf010bf9c3fb101ee206494139b73a5585b7
nzai <nzaih18@gmail.com>
2023-06-09 11:30:38 +0300

    fix some stuff

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default async function Home() {
         <div className="">
           <Navbar href={`/`} />
           <div className="flex items-center gap-1">
-            <Avatar image={user?.image!} />
+            <Avatar image={user?.image!} name={user?.name!} />
             <AddPosts />
           </div>
           {/* @ts-ignore */}
